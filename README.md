1df1d1c5df0704f6c7277ecc0cbed2fb24053c38
anjaliasha123 <anjaliasha95@gmail.com>
2024-04-22 08:29:09 -0500

    saving blob objects

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 This is a project that builds a Git clone from scratch
 <ol>
     <li> Implement git init to initialize a git repository by creating a .git directory and some files inside it.</li>
+    <li> Implementing reading blobs. git hash-object. hash-object: Save object</li>
 </ol>
--- src/main/java/com/anjalicodes/building/git/clone/BuildingGitCloneApplication.java
+++ src/main/java/com/anjalicodes/building/git/clone/BuildingGitCloneApplication.java
@@ -1,11 +1,38 @@
 package com.anjalicodes.building.git.clone;
 
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 public class BuildingGitCloneApplication {
+	private static String computeFileHash(File file) {
+		String sha1 = null;
+		MessageDigest digest;
+		try{
+			digest = MessageDigest.getInstance("SHA-1");
+			FileInputStream inputStream = new FileInputStream(file);
+			byte[] buffer = new byte[8192];
+			int read = 0;
+			while((read = inputStream.read(buffer)) != -1) digest.update(buffer, 0, read);
+			inputStream.close();
+			byte[] hashedBytes = digest.digest();
+			StringBuilder hashedString = new StringBuilder();
+			for(byte b: hashedBytes) hashedString.append(String.format("%02x", b));
+			return hashedString.toString();
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("SHA-1 algorithm instance not found");
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: "+file.getName());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return sha1;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Logs from your program will appear here!");
@@ -13,7 +40,7 @@ public class BuildingGitCloneApplication {
 
 		switch (command) {
 			case "init" : {
-				final File root = new File("output/.git");
+				final File root = new File(".mygit");
 				new File(root, "objects").mkdirs();
 				new File(root, "refs").mkdirs();
 				final File head = new File(root, "HEAD");
@@ -26,6 +53,33 @@ public class BuildingGitCloneApplication {
 					throw new RuntimeException(e);
 				}
 			}
+			break;
+			case "hash-object":{
+				try{
+					//get the path of the file that is to be stored
+					//read it from command line
+					String path = Paths.get("").toAbsolutePath()
+							.toString()+"\\"+args[1];
+//					System.out.println("Path is: "+path);
+					//read the contents of the file and hash it
+					String oid = computeFileHash(new File(path));
+
+					//Store the file under ".ugit/objects/{the SHA-1 hash}"
+					String outPath = Paths.get("").toAbsolutePath().toString()+"\\.mygit\\objects\\";
+					File outFile = new File(outPath+oid);
+					FileOutputStream outputStream = new FileOutputStream(outFile);
+					outputStream.write(oid.getBytes());
+					outputStream.close();
+					System.out.println("OID: "+oid);
+				}catch(ArrayIndexOutOfBoundsException e){
+					System.out.println("Please enter the path to the file");
+				} catch (FileNotFoundException e) {
+					System.out.println("Did not find the file in path \n"+e.getMessage());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			break;
 			default : System.out.println("Unknown git command: " + command);
 		}
 	}
